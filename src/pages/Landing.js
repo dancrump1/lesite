4d5136263456695c935b15ba3ad8511e30cca552
dancrump1 <danielcrump@outlook.com>
2019-04-07 22:55:03 -0400

    making moves on the mobile front

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -21,7 +21,7 @@ grid-template-rows: 1fr 10fr;
 grid-gap: 10px;
 `;
 
-const Nameplate = Styled.div`
+const NameplateD = Styled.div`
 background-color: #FFFCE4;
 height: 100vh;
 width: 80vw;
@@ -40,21 +40,50 @@ transform: rotate(90deg);
 margin: 15px;
 `;
 
+const BackimgM = Styled.div`
+background: linear-gradient(#5614B0, #1fddff) no-repeat center center fixed;
+background-attachment: fixed;
+height: 100vh;
+position: relative;
+`;
+
+const NameplatePicM = Styled.img.attrs({ src: MountainPic })`
+height: auto;
+width: 75%;
+transform: rotate(90deg);
+margin: 0 auto;
+`;
+
+const NameplateM = Styled.div`
+background-color: #FFFCE4;
+height: auto;
+width: 80vw;
+border-radius: 25px;
+text-align: center;
+overflow: hidden;
+display: grid;
+grid-gap: 15vh;
+position: absolute;
+top: 15vh;
+left: 10vw;
+`;
+
 function Landing() {
     return (
         <div>
+
             <Desktop>
                 <Backimg>
 
                     <Nav />
 
-                    <Nameplate>
+                    <NameplateD>
                         <h1>Hi my name is Daniel Crump and welcome to my website</h1>
                         <div style={{ overflow: "hidden" }}>
                             <NameplatePic />
                         </div>
                         <h3>This site is built in React, and is an example of my front end design capabilities!</h3>
-                    </Nameplate>
+                    </NameplateD>
 
                 </Backimg>
 
@@ -67,14 +96,14 @@ function Landing() {
             </Tablet>
 
             <Mobile>
-                <Backimg>
-                    <Nameplate>Hi mah nem is dan
-                     <br></br>
-                        <NameplatePic />
-                    </Nameplate>
-                </Backimg>
+                <BackimgM>
+                    <NameplateM>
+                        <h1>Hi mah nem is dan</h1>
+                        <NameplatePicM />
+                        <h3>This is the mobile version of my site!</h3>
+                    </NameplateM>
+                </BackimgM>
             </Mobile>
-
 
         </div>
     )
