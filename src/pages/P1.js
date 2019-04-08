7bd3897f28f86bfca763eb1add7a749f79b2cd8a
dancrump1 <danielcrump@outlook.com>
2019-04-07 22:30:17 -0400

    working on Carousel content fitting inside and the overall layout

--- src/pages/P1.js
+++ src/pages/P1.js
@@ -13,13 +13,17 @@ width: 100%;
 const SiteGrid = Styled.div`
 display: grid;
 grid-template-areas: 
-'Nav Nav'
-'E2 E3'
+'Hea Hea'
+'E2 E2'
+'E3 E3'
 'E5 E6'
 'E4 E4';
 grid-template-columns: 1fr 1fr;
-grid-template-rows: 1fr 4fr 4fr 1fr;
-grid-gap: 10px
+grid-template-rows: 1fr 14fr 14fr 14fr 1fr;
+`;
+
+const E1 = Styled.div`
+grid-area: Hea;
 `;
 
 const E2 = Styled.div`
@@ -45,6 +49,7 @@ margin: 10px;
 const E6 = Styled.div`
 grid-area: E6;
 margin: 10px;
+text-align: center;
 `;
 
 
@@ -56,85 +61,76 @@ class P1 extends React.Component {
     render() {
         return (
             <Backimg>
-            <SiteGrid>
-                <Nav className="Nav" />
-                <E2>
-                    <h1>My experience with youtube</h1>
+                <SiteGrid>
+                    <E1>
+                        <Nav />
+                    </E1>
+                    <E2>
+                        <h1>My experience with youtube</h1>
 
-                    <Carousel showThumbs={ false }>
-                        <div>
-                            <iframe
-                                id="player"
-                                title="vid1"
-                                type="text/html"
-                                width="100%"
-                                height="600px"
-                                src="https://www.youtube.com/embed/ZFWLSzuvKPE"
-                                frameBorder="0">
-                            </iframe>
-                        </div>
+                        <Carousel showThumbs={false}>
+                            <div className="videoWrapper">
+                                <iframe
+                                    id="player"
+                                    title="vid1"
+                                    type="text/html"
+                                    src="https://www.youtube.com/embed/ZFWLSzuvKPE"
+                                    frameBorder="0">
+                                </iframe>
+                            </div>
 
-                        <div>
-                            <iframe
-                                id="player"
-                                type="text/html"
-                                title="vid2"
-                                width="100%"
-                                height="600px"
-                                src="https://www.youtube.com/embed/Unx3oCpg2z8"
-                                frameBorder="0"
-                                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                            >
-                            </iframe>
-                        </div>
-                    </Carousel>
-                </E2>
-                <E3>
-                    <h1>Full Twitch Vods</h1>
-                    <Carousel showThumbs={ false }>
-                        <div>
-                            <iframe
-                                src="https://player.twitch.tv/?channel=justlowfps&autoplay=false"
-                                height="600px"
-                                width="100%"
-                                frameBorder="<frameborder>"
-                                scrolling="<scrolling>"
-                                title="Iframe1">
-                            </iframe>
-                        </div>
-                        <div>
-                            <iframe
-                                src="https://player.twitch.tv/?video=403256543&autoplay=false"
-                                height="600px"
-                                width="100%"
-                                frameBorder="<frameborder>"
-                                scrolling="<scrolling>"
-                                title="Iframe2">
-                            </iframe>
-                        </div>
-                    </Carousel>
-                </E3>
-                <E5>
-                <h1>Only the BEST clips</h1>
+                            <div className="videoWrapper">
+                                <iframe
+                                    id="player"
+                                    type="text/html"
+                                    title="vid2"
+                                    src="https://www.youtube.com/embed/Unx3oCpg2z8"
+                                    frameBorder="0"
+                                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                                >
+                                </iframe>
+                            </div>
+                        </Carousel>
+                    </E2>
+                    <E3>
+                        <h1>Full Twitch Vods</h1>
+                        <Carousel showThumbs={false}>
+                            <div className="videoWrapper">
+                                <iframe
+                                    src="https://player.twitch.tv/?channel=justlowfps&autoplay=false"
+                                    title="Iframe1">
+                                </iframe>
+                            </div>
+                            <div className="videoWrapper">
+                                <iframe
+                                    src="https://player.twitch.tv/?video=403256543&autoplay=false"
+                                    title="Iframe2">
+                                </iframe>
+                            </div>
+                        </Carousel>
+                    </E3>
+                    <E5>
+                        <h1>Only the BEST clips</h1>
 
-                    <Carousel showThumbs={ false }>
-                        <div>
-                            <iframe
-                                src="https://clips.twitch.tv/embed?clip=SourBlushingEggplantTTours&autoplay=false"
-                                height="600px"
-                                width="100%"
-                                frameBorder="<frameborder>"
-                                scrolling="<scrolling>"
-                                title="Iframe3">
-                            </iframe>
+                        <Carousel showThumbs={false}>
+                            <div>
+                                <iframe
+                                    src="https://clips.twitch.tv/embed?clip=SourBlushingEggplantTTours&autoplay=false"
+                                    height="600px"
+                                    width="100%"
+                                    frameBorder="<frameborder>"
+                                    scrolling="<scrolling>"
+                                    title="Iframe3">
+                                </iframe>
 
-                        </div>
-                    </Carousel>
-                </E5>
-                <E6>
-                </ E6>
-                <E4 />
-            </SiteGrid>
+                            </div>
+                        </Carousel>
+                    </E5>
+                    <E6>
+                        <h1>Social media for JLOW</h1>
+                    </ E6>
+                    <E4 />
+                </SiteGrid>
             </Backimg>
         )
     }
--- src/pages/PB.js
+++ src/pages/PB.js
@@ -6,9 +6,9 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import SideNav from '../components/SideNav';
 import MediaQuery from 'react-responsive';
 
-const Desktop = props => <MediaQuery { ...props } minWidth={ 1281 } />;
-const Tablet = props => <MediaQuery { ...props } minWidth={ 768 } maxWidth={ 1280 } />;
-const Mobile = props => <MediaQuery { ...props } maxWidth={ 767 } />;
+const Desktop = props => <MediaQuery {...props} minWidth={1281} />;
+const Tablet = props => <MediaQuery {...props} minWidth={768} maxWidth={1280} />;
+const Mobile = props => <MediaQuery {...props} maxWidth={767} />;
 
 const Backimg = Styled.div`
 background-image: linear-gradient(#5614B0, #1fddff);
@@ -24,7 +24,8 @@ grid-template-areas:
 'SideNav E3 E3'
 'E4 E4 E4';
 grid-template-columns: 1fr 1fr 3fr;
-grid-gap: 60px;
+grid-template-rows: 1fr 10fr 10fr;
+grid-gap: 30px;
 `;
 
 const E2 = Styled.div`
@@ -37,7 +38,6 @@ margin: 15px
 const E3 = Styled.div`
 grid-area: E3;
 background: green;
-margin: 0 auto;
 border: 2px solid palevioletred;
 border-radius: 3px;
 margin: 15px
@@ -69,97 +69,93 @@ class PB extends React.Component {
       <div>
         <Desktop>
           <Backimg>
-          <SiteGrid>
-            <E1>
-              <Nav className="Nav" />
-            </E1>
+            <SiteGrid>
+              <E1>
+                <Nav className="Nav" />
+              </E1>
 
-            <E5>
-              <SideNav className="SideNav" />
-            </E5>
+              <E5>
+                <SideNav className="SideNav" />
+              </E5>
 
-            <E2 id="E2">
-              <Carousel showThumbs={ false }>
-                <div>
-                  <iframe 
-                  src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fdan.crump.9%2Fvideos%2F2231439123545996%2F&show_text=0&" 
-                  title="Breakout" 
-                  style={{width: "100%", height: "100%"}}
-                  scrolling="no" 
-                  frameBorder="0"  
-                  allowFullScreen={ true }>
-                  </iframe>
-                </div>
-                <div>
-                  <iframe 
-                  title="Iframe1" 
-                  src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fdan.crump.9%2Fvideos%2F2231437240212851%2F&show_text=0&" 
-                  width="100%"
-                  height="600px"
-                  scrolling="no" 
-                  frameBorder="0" 
-                  allowFullScreen={true}>
-                  </iframe>
-                </div>
-              </Carousel>
-            </E2>
+              <E2 id="E2">
+                <Carousel showThumbs={false}>
+                  <div className="videoWrapper">
+                    <iframe
+                      id="player"
+                      title="Breakout"
+                      type="text/html"
+                      src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fdan.crump.9%2Fvideos%2F2231439123545996%2F&show_text=0&"
+                      frameBorder="0"
+                    >
+                    </iframe>
+                  </div>
+                  <div className="videoWrapper">
+                    <iframe
+                      title="Iframe1"
+                      src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fdan.crump.9%2Fvideos%2F2231437240212851%2F&show_text=0&"
 
-            <E3 id="E3">
-              <Carousel autoPlay={ false } showArrows={ true } showThumbs={ false }>
-                <div>
-                  <iframe
-                   title="Iframe2"
-                   src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fdan.crump.9%2Fposts%2F2229967180359857%3A0"
-                   width="100%"
-                   height="600px"
-                  >
-                  </iframe>
-                </div>
-                <div>
-                  <iframe 
-                  title="Iframe3" 
-                  src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fphoto.php%3Ffbid%3D2231273116895930%26set%3Da.971308412892413%26type%3D3" 
-                  width="100%"
-                  height="600px"
-    scrolling="no" 
-                  frameBorder="0"  
-                  allow="encrypted-media">
-                  </iframe>
-                </div>
-                <div>
-                  <iframe 
-                  title="Iframe4" 
-                  src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fosgpaintball%2Fphotos%2Fa.10154228082491192%2F10154228090986192%2F%3Ftype%3D3" 
-                  width="100%"
-                  height="600px"
-                  >
-                  </iframe>                    
-                </div>
-                <div>
-                  <iframe 
-                  title="Iframe5" 
-                  src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fphoto.php%3Ffbid%3D2231273123562596%26set%3Da.971308412892413%26type%3D3" 
-                  width="100%"
-                  height="600px"
-                  >
-                  </iframe>
-                </div>
-                <div>
-                  <iframe 
-                  title="Iframe6" 
-                  src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fphoto.php%3Ffbid%3D2231273146895927%26set%3Da.971308412892413%26type%3D3" 
-                  width="100%"
-                  height="600px"
-                  >
-                  </iframe>
-                </div>
-              </Carousel>
-            </E3>
+                      scrolling="no"
+                      frameBorder="0"
+                      allowFullScreen={true}>
+                    </iframe>
+                  </div>
+                </Carousel>
+              </E2>
 
-            <E4 id="E4">
+              <E3 id="E3">
+                <Carousel autoPlay={false} showArrows={true} showThumbs={false}>
+                  <div className="picWrapper">
+                    <iframe
+                      title="Iframe2"
+                      src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fdan.crump.9%2Fposts%2F2229967180359857%3A0"
+                      type="text/html"
+                      frameBorder="0"
+                    >
+                    </iframe>
+                  </div>
+                  <div className="picWrapper">
+                    <iframe
+                      title="Iframe3"
+                      src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fphoto.php%3Ffbid%3D2231273116895930%26set%3Da.971308412892413%26type%3D3%"
+                      frameBorder="0"
+                    >
+                    </iframe>
+                  </div>
+                  <div className="picWrapper">
+                    <iframe
+                      title="Iframe4"
+                      src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fosgpaintball%2Fphotos%2Fa.10154228082491192%2F10154228090986192%2F%3Ftype%3D3"
+                      width="100%"
+                      height="600px"
+                    >
+                    </iframe>
+                  </div>
+                  <div className="picWrapper">
+                    <iframe
+                      title="Iframe5"
+                      src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fphoto.php%3Ffbid%3D2231273123562596%26set%3Da.971308412892413%26type%3D3"
+                      width="100%"
+                      height="600px"
+                    >
+                    </iframe>
+                  </div>
+                  <div className="picWrapper">
+                    <iframe
+                      title="Iframe6"
+                      src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fphoto.php%3Ffbid%3D2231273146895927%26set%3Da.971308412892413%26type%3D3"
+                      width="100%"
+                      height="600px"
+                    >
+                    </iframe>
+                  </div>
+                </Carousel>
+              </E3>
 
-            </E4>
-          </SiteGrid>
+              <E4 id="E4">
+
+              </E4>
+            </SiteGrid>
           </Backimg>
         </Desktop>
 
